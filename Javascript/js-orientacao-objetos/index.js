c47e636a4fedacaf41debdd01d3cf517f75e1830
Mario <mario.ohashi@gmail.com>
2022-06-28 17:22:44 -0300

    Update index.js

--- Javascript/js-orientacao-objetos/index.js
+++ Javascript/js-orientacao-objetos/index.js
@@ -4,9 +4,15 @@ import { Diretor } from "./Funcionario/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Mario", 10000, 1234567890);
-diretor.cadastrarSenha("1234");
+diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Joao", 5000, 9876543210);
+diretor.cadastrarSenha("1234");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "1234");
+const cliente = new Cliente("Lais", 789123460, "456");
 
-console.log(estaLogado);
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "1234");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
