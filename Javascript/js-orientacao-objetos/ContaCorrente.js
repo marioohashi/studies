ebde59f157d3ac3db8b1d5f2a807b9b66cd7f1cb
Mario <mario.ohashi@gmail.com>
2022-06-28 16:18:13 -0300

    Update ContaCorrente.js

--- Javascript/js-orientacao-objetos/ContaCorrente.js
+++ Javascript/js-orientacao-objetos/ContaCorrente.js
@@ -1,20 +1,14 @@
-import { Cliente } from "./Cliente.js";
 import { Conta } from "./Conta.js";
 
 export class ContaCorrente extends Conta{
     static numeroDeContas = 0;   
     constructor(cliente, agencia){
-        super(0,cliente,agencia);
+        super(0,cliente,agencia);   
         ContaCorrente.numeroDeContas += 1;
     }
-
-sacar(valor){
-    let taxa = 1.1;
-
-    const valorSacado = taxa*valor;
-    if(this._saldo >= valorSacado){
-        this._saldo -= valorSacado;
-        return valorSacado;
+    // sobreescrevendo o comportamento de sacar
+    sacar(valor){
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
     }
-}
 }
