52904f71e49b082599a6c6f0eb595b58750fe37e
Mario <mario.ohashi@gmail.com>
2022-06-28 21:17:53 -0300

    Create main.js

---
+++ Javascript/aluramidi/main.js
@@ -0,0 +1,30 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento != null && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert("Elemento não encontrado");
+  }
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (event) {
+    if (event.code == "Space" || event.code == "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
