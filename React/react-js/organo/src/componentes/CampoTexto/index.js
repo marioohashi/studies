73ac60bfef617e3c5576f9fb215227fb5d505c65
Mario <mario.ohashi@gmail.com>
2022-06-29 17:12:29 -0300

    Create index.js

---
+++ React/react-js/organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,13 @@
+import "./CampoTexto.css";
+
+const CampoTexto = (props) => {
+  const placeholderModificado = `${props.placeholder}...`;
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input placeholder={placeholderModificado} />
+    </div>
+  );
+};
+
+export default CampoTexto;
