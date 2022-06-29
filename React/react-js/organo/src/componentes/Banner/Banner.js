ea4cf4081d73ab7f79d61351bfaba770e57300f5
Mario <mario.ohashi@gmail.com>
2022-06-29 17:12:22 -0300

    Update Banner.js

--- React/react-js/organo/src/componentes/Banner/Banner.js
+++ React/react-js/organo/src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import "./Banner.css";
 
-function Banner() {
+export const Banner = () => {
   // JSX
   return (
     <header className="banner">
@@ -10,6 +10,4 @@ function Banner() {
       />
     </header>
   );
-}
-
-export default Banner;
+};
