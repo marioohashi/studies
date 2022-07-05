49d1a6669e33a5dda0761c98a281a3e1aef9d604
Mario <mario.ohashi@gmail.com>
2022-07-05 16:24:18 -0300

    Update index.js

--- React/react-js/organo/src/componentes/CampoTexto/index.js
+++ React/react-js/organo/src/componentes/CampoTexto/index.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
   const placeholderModificado = `${props.placeholder}...`;
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={placeholderModificado} />
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificado}
+      />
     </div>
   );
 };
